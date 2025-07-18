a2bece4471eaf0e1d736fedd49a622e2a9c7122f
Bruno Tenório <tenoriobn@gmail.com>
2025-07-18 19:46:52 -0300

    feat: :sparkles: 18/07/25 - adiciona projeto base com Nuxt e Tailwind CSS

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-amber-500 h-dvh">
+    <NuxtRouteAnnouncer />
+    <h2>teste</h2>
+    <p class="text-3xl font-bold underline text-blue-700">Eis aqui um texto</p>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  css: ['@/assets/css/main.css',],
+  vite: {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname),
+      },
+    },
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
